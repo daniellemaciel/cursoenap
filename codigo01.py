ab5d173d29a7b82bc19e76453651d8aac4b78a71
daniellemaciel <danielle.maciel@gmail.com>
2024-11-13 11:38:56 -0300

    codigo01.py

--- codigo01.py
+++ codigo01.py
@@ -12,6 +12,6 @@ st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
 
-x = st.select_slider('Selecione seu grau de satisfação',0,100)
+x = st.slider('Selecione seu grau de satisfação',0,100)
 st.write('O grau de satisfação escolhido foi: ')
 st.write(x)
