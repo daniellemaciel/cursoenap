b182c90e611b44573b66d818f07da31136fefd58
daniellemaciel <danielle.maciel@gmail.com>
2024-11-14 10:00:07 -0300

    aula09-20241114.py

---
+++ aula09-20241114.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'nomeServidor': ['Adriana', 'Thais', 'Samara'],
+    'salario': [10000, 25000, 20000]
+})
+
+st.write("Criando uma tabela")
+st.write(df)
+
+opcao = st.selectbox(
+    'Qual servidor você gostaria de selecionar?',
+    options=["Selecione..."] + df['nomeServidor'].tolist()
+)
+
