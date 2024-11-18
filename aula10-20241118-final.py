688ef496ca5816c90fba9e059b3e3a36627da00b
daniellemaciel <danielle.maciel@gmail.com>
2024-11-18 10:57:53 -0300

    Update aula10-20241118-final.py

--- aula10-20241118-final.py
+++ aula10-20241118-final.py
@@ -36,3 +36,19 @@ dfEstados = pd.DataFrame({
     'siglaUf': ocorrencias.index,
     'quantidade': ocorrencias.values}
     )
+
+
+#item 7
+#total de homens
+totalHomens = df_homens['id'].count()
+st.metric('Total de Homens', totalHomens)
+#total de mulheres
+totalMulheres = df_mulheres['id'].count()
+st.metric('Total de Mulheres', totalMulheres)
+st.write('Total de deputadas do sexo ' + opcao)
+st.bar_chart(dfEstados,
+             x = 'siglaUf',
+             y = 'quantidade',
+             x_label='Siglas dos estados',
+             y_label='Quantidade de deputados')
+st.dataframe(dfFiltrado)
