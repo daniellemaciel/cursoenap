ce437a1d446290db4d6027e13300c6ea7d440424
daniellemaciel <danielle.maciel@gmail.com>
2024-11-18 10:36:26 -0300

    Aula final do nível 3 Python
    aula10-20241118-final.py

---
+++ aula10-20241118-final.py
@@ -0,0 +1,24 @@
+#importando os dados das deputadas
+url_mulheres = 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=F&ordem=ASC&ordenarPor=nome'
+resposta_mulheres = rq.get(url_mulheres)
+dadosJSONmulheres = resposta_mulheres.json()
+df_mulheres = pd.DataFrame(dadosJSONmulheres['dados'])
+df_mulheres['sexo'] = 'F'
+
+#importando os dados dos deputados
+url_homens = 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=M&ordem=ASC&ordenarPor=nome'
+resposta_homens = rq.get(url_homens)
+dadosJSONhomens = resposta_homens.json()
+df_homens = pd.DataFrame(dadosJSONhomens['dados'])
+df_homens['sexo'] = 'M'
+
+#juntar os dois dfs
+df_total = pd.concat([df_mulheres, df_homens])
+
+#Filtrando df por sexo
+#inserindo um selectbox
+opcao = st.selectbox(
+    'Qual o sexo?',
+     df_total['sexo'].unique())
+dfFiltrado = df_total[df_total['sexo'] == opcao]
+st.title('Deputados do sexo ' + opcao)
